5e424a75854b8d67a3d49bf248b712d0eb6536cd
jose rasgado <jrasgado01@hotmail.com>
2024-06-27 22:59:06 -0600

    Added send email functionality and added to the main file

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+import glob
+import os
+
 import cv2
 import time
+import os
 from emailing import send_email
 
 video = cv2.VideoCapture(0)  # Initialize the video camera with the 1st camera
 time.sleep(1)
 first_frame = None
 status_list = []
+count = 1
+
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove()
+
+
 while True:
     status = 0
     check, frame = video.read()
+
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Transform the frame intro gray
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
 
@@ -31,12 +45,18 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame) # Create images only when an object enters the video
+            count += 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:] # Picks the last 2 items
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(image_with_object)
+        clean_folder()
 
     print(status_list)
 
